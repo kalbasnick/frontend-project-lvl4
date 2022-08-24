e4757d5b47ae4e19e7ab598de81a3415c22a1701
Kirichenko Nikita <knick@MacBook-Pro.local>
2022-08-24 19:58:05 +0300

    second step of the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav> */}
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, 'From 3 to 20 symbols')
+    .max(20, 'From 3 to 20 symbols')
+    .required('Required'),
+  password: Yup.string()
+    .min(6, 'At least 6 symbols')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
+
+function Login() {
+  return <div>
+  <h1>Войти</h1>
+  <Formik
+    initialValues={{
+      username: '',
+      password: '',
+    }}
+    validationSchema={SignupSchema}
+    onSubmit={values => {
+      // same shape as initial values
+      console.log(values);
+    }}
+  >
+    {({ errors, touched }) => (
+      <Form>
+      <Field id="username" name="username" placeholder="Ваш ник" />
+      {errors.username && touched.username ? (
+             <div>{errors.username}</div>
+           ) : null}
+      <Field id="password" name="password" placeholder="Пароль" />
+      {errors.password && touched.password ? (
+             <div>{errors.password}</div>
+           ) : null}
+      <button type="submit">Войти</button>
+    </Form>
+    )}
+  </Formik>
+</div>;
+}
+
+function NoMatch() {
+  return <h1>Error 404. Page not found</h1>
+}
